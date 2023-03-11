cc690c981e57078a763e381d72290a65ea493f43
Lasha Kvachadze <98225237+Kvachi4@users.noreply.github.com>
2023-03-11 21:37:36 +0400

    Add files via upload
    Exercise 7

---
+++ deep.py
@@ -0,0 +1,10 @@
+userAnswer = input("What is the Answer to the Great Question of Life, the Universe and Everything? ")
+
+if userAnswer == "42":
+    print("Yes")
+elif userAnswer == "forty-two":
+    print("Yes")
+elif userAnswer == "forty two":
+    print("Yes")
+else:
+    print("No")
